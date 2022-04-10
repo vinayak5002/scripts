6f60cf1ed6494810a7f91483ea0a3d3f1899ed34
vinayak5002 <mailvizzard@gmail.com>
2022-04-10 08:57:13 -0700

    file read

---
+++ sys/fileOps/fileRead.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main(int argc, char** argv)
+{
+	char *c;
+	int fd, sz;
+	//fd is called file descripter same as file pointer
+}
